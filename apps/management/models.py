23fc7b73f6b8158a4464d9cc37bc6aaa5e228079
Sam Jamal <sammy@ultra.me>
2019-03-28 11:56:56 -0700

    updates for new django package

--- apps/management/models.py
+++ apps/management/models.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from apps.login.models import Patient
+from login.models import Patient
 from django.db import models
 
 
--- apps/management/urls.py
+++ apps/management/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'management'
 urlpatterns = [
     url(r'^$', views.index, name="index"),
     url(r'^create', views.create, name="create"),
